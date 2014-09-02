9c52fc622e0ba09e0f135e1bcf9400025673c47e
Andre Oliveira <andre.lemaf@gmail.com>
2014-09-02 12:05:15 -0300

    criacao inicial da classe conta

--- src/br/caelum/financas/modelo/Conta.java
+++ src/br/caelum/financas/modelo/Conta.java
@@ -5,4 +5,29 @@ import javax.persistence.*;
 @Entity
 public class Conta {
 
+	@Id
+	private Integer id;
+	
+	private String titular;
+	private String agencia;
+	private String numero;
+	
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public String getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 }
